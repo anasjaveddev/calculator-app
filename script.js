59b421b0e24514149baa2c74298debc5d5b128f1
Anas Javed <anasmughalonline@gmail.com>
2026-04-01 16:37:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function getValues() {
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
+  return { a, b };
+}
+
+function add() {
+  let { a, b } = getValues();
+  document.getElementById("result").innerText = "Result: " + (a + b);
+}
+
+function subtract() {
+  let { a, b } = getValues();
+  document.getElementById("result").innerText = "Result: " + (a - b);
+}
+
+function multiply() {
+  let { a, b } = getValues();
+  document.getElementById("result").innerText = "Result: " + (a * b);
+}
+
+function divide() {
+  let { a, b } = getValues();
+  if (b === 0) {
+    document.getElementById("result").innerText = "Cannot divide by zero";
+  } else {
+    document.getElementById("result").innerText = "Result: " + (a / b);
+  }
+}
